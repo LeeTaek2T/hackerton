c9e501d0704749141c12bcede4a49e432fe6a1b2
inwoo0109 <inwoo0109@naver.com>
2023-09-16 07:39:57 +0900

    sub03_1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.module.css';
-import Header from './component/header/header'
-import Footer from './component/footer/footer'
-import Main from './component/main/main'
+import Header from './component/header/header';
+import Footer from './component/footer/footer';
+import Main from './component/main/main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,9 +9,7 @@ function App() {
     <div className={StyleSheet.App}>
       <BrowserRouter>
         <Routes>
-          <Route exact path='/'
-            element={<Main></Main>}>
-          </Route>
+          <Route exact path="/" element={<Main></Main>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/component/sub03_1/sub03_1.jsx
@@ -0,0 +1,58 @@
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import styles from './main.module.css';
+import myProfilePhoto from '../../source/tree.jpg';
+import sponsored1 from '../../source/Sponsored1.jpg';
+import sponsored2 from '../../source/Sponsored2.jpg';
+
+const Main = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.headerDiv}>
+        <Header></Header>
+      </div>
+      <div className={styles.HeadLineDiv}>
+        <b>나의 후원</b>
+      </div>
+      <div className={styles.MyProfile}>
+        <div className={styles.MyPhoto}>
+          <img src={myProfilePhoto} className={styles.MyProfilePhoto} />
+        </div>
+        <div className={styles.MyInfo}>
+          <div className={styles.MyName}>
+            <b>상수우</b>
+          </div>
+          <div className={styles.SponAmount}>누적후원금 210,000원</div>
+          <button className={styles.ModifyInfo}>
+            <b>회원정보 수정</b>
+          </button>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.ListHeadTxt}>
+        <b>후원중인새싹들</b>
+      </div>
+      <div className={styles.Sponsored}>
+        <div className={styles.SponsoredImgSpace}>
+          <img src={sponsored1} className={styles.SponsoredPhoto} />
+        </div>
+        <div className={styles.SponsoredInfo}>
+          <div>
+            <b>오수연</b>
+            <br />
+            후원시작일 : 20203.07.02.
+            <br />월 후원금액 : 20,000원
+          </div>
+          <button className={styles.LetterButton}>편지함</button>
+        </div>
+      </div>
+      <hr className={styles.BetweenSponsored} />
+      <div className={styles.Sponsored}></div>
+      <div div className={styles.footerDiV}>
+        <Footer></Footer>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
